e1c75719a83e03a7dcedd216228c6361b2683eb4
devopsjourney1 <34145011+devopsjourney1@users.noreply.github.com>
2022-11-30 09:41:42 -0800

    Update iam.sh

--- iam.sh
+++ iam.sh
@@ -1,5 +1,8 @@
-
-# Upload a ssh key pair
+# SSH
+## Upload a ssh key pair
 aws iam upload-ssh-public-key \
 --user-name gitUser \
 --ssh-public-key-body file://~/.ssh/id_rsa.pub
+
+## Display it
+KEYID=$(aws iam list-ssh-public-keys --user-name gitUser | jq -r '.[] | .[] | .SSHPublicKeyId') && echo $KEYID
